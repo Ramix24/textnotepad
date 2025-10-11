a2e818f35bf6cf69395793ec0733f5465cda00a9
Ramix24 <156318974+Ramix24@users.noreply.github.com>
2025-10-11 21:28:30 +0200

    Version bump to 0.5.129

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -5,7 +5,7 @@
  */
 
 // This version is automatically updated by scripts/auto-version.js
-export const APP_VERSION = '0.5.128'
+export const APP_VERSION = '0.5.129'
 export const APP_NAME = 'textnotepad'
 
 /**
